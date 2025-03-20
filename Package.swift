41796970e017aaa8a9cce755b6844700999d3c3c
Lsong <song940@gmail.com>
2025-03-20 11:45:49 +0800

    update

--- Package.swift
+++ Package.swift
@@ -6,7 +6,10 @@ import PackageDescription
 let package = Package(
     name: "SwiftUIX",
     platforms: [
-        .iOS(.v16)
+        .iOS(.v16),
+        .macOS(.v12),
+        .watchOS(.v8),
+        .tvOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
